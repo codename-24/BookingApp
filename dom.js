0c2d1ce89d1a4622e08b0cdb3859951b52aa1cc7
codename-24 <vijay2495@gmail.com>
2022-03-27 15:23:02 +0530

    buttoncommit

--- dom.js
+++ dom.js
@@ -1,46 +1,16 @@
-//parent
-var itemList = document.querySelector('#items');
-console.log(itemList.parentElement);
-itemList.parentElement.style.backgroundColor = '#f4f4f4';
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
-//childNodes
-console.log(itemList.childNodes);
-console.log(itemList.firstChild);
-console.log(itemList.firstElementChild);
+itemList.addEventListener('click', removeItem);
 
-console.log(itemList.lastChild);
-console.log(itemList.lastElementChild);
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+  }
+}
 
-//siblings
-console.log(itemList.nextSibling);
-console.log(itemList.nextElementSibling);
 
-console.log(itemList.previousSibling);
-
-//create
-var newDiv = document.createElement('div');
-newDiv.className = 'hello';
-newDiv.id = 'hello1';
-newDiv.setAttribute('title','Hello Div');
-var newDivText = document.createTextNode('Hello World');
-newDiv.appendChild(newDivText);
-var container= document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
-console.log(newDiv);
-container.insertBefore(newDiv, h1);
-newDiv.style.fontSize = '30px';
-
-var newDiv2 = document.createElement('div');
-newDiv2.className = 'hello1';
-newDiv2.id = 'hello2';
-newDiv.setAttribute('title','Hello Div2');
-var newDivText2 = document.createTextNode('Hello 1');
-newDiv2.appendChild(newDivText2);
-
-var container2= document.querySelector('.title');
-var h2 = document.querySelector('header h2');
-console.log(newDiv2);
-container2.insertBefore(newDiv2, h2);
-newDiv2.style.fontSize = '30px';
 
 
