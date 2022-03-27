02fd8d33aef80726df0b82baec0e5d7ccf508bb3
codename-24 <vijay2495@gmail.com>
2022-03-27 17:33:31 +0530

    appcommit

--- dom.js
+++ dom.js
@@ -1,9 +1,11 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 var filter = document.getElementById('filter');
-
+var count = 0;
 itemList.addEventListener('click', removeItem);
 filter.addEventListener('keyup', filterItems);
+form.addEventListener('submit', addItem);
+
 
 function removeItem(e){
   if(e.target.classList.contains('delete')){
@@ -25,6 +27,25 @@ function filterItems(e){
       }
     });
   }
+  function addItem(e){
+    e.preventDefault();
+    
+    const newItems = [];
+    var newItem = document.getElementById('item').value;
+    
+    count++;
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+    itemList.appendChild(li);
+    for(let j=0;j<count;j++)
+    {
+    newItems.push(newItem[j]);
+    localStorage.setItem('itemName'+[j],JSON.stringify(newItems));
+    }
+    
+  }
+ 
 
 
 
