55a8ba0d89cf1238cd642f5b65dea2b4da5f90b5
codename-24 <vijay2495@gmail.com>
2022-03-27 10:25:59 +0530

    tagnamecommit

--- dom.js
+++ dom.js
@@ -5,3 +5,7 @@ items[2].style.backgroundColor = "green";
 for(var i=0; i<items.length;i++){
     items[i].style.fontWeight = 'bold';
 }
+var li = document.getElementsByTagName('li');
+li[4].style.color = 'green';
+console.log(li[4]);
+
