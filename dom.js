cfa0dc9c2c84c4bc953d211cea4f29ce9b5afdc8
codename-24 <vijay2495@gmail.com>
2022-03-27 11:02:18 +0530

    querycommit

--- dom.js
+++ dom.js
@@ -1,7 +1,7 @@
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[2]);
-items[2].style.backgroundColor = "green";
+
 for(var i=0; i<items.length;i++){
     items[i].style.fontWeight = 'bold';
 }
@@ -9,3 +9,19 @@ var li = document.getElementsByTagName('li');
 li[4].style.color = 'green';
 console.log(li[4]);
 
+var qs = document.querySelector('.list-group-item');
+qs.style.backgroundColor = 'red';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = '#f4f4f4';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility = 'hidden';
+
+var qsa = document.querySelectorAll('.list-group-item');
+qsa[1].style.color = 'red';
+
+var odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+for(var j=0;j<odd.length;j++){
+    odd[j].style.backgroundColor='green';
+}
