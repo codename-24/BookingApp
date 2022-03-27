fe2464986df1cf933c883538e4a0c99dad0eb24d
codename-24 <vijay2495@gmail.com>
2022-03-27 15:58:46 +0530

    filtercommit

--- dom.js
+++ dom.js
@@ -3,6 +3,7 @@ var itemList = document.getElementById('items');
 var filter = document.getElementById('filter');
 
 itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItems);
 
 function removeItem(e){
   if(e.target.classList.contains('delete')){
@@ -10,6 +11,20 @@ function removeItem(e){
       itemList.removeChild(li);
   }
 }
+function filterItems(e){
+ 
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
 
 
 
